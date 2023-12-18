41c98fed8afa5307dad1be0b19f7ef45de86efe9
Win 11 <rawadabuzed.1999@gmail.com>
2023-12-18 21:03:45 +0200

    Initial commit

---
+++ src/main/java/com/example/Second_Projict_SpringBoot/SecondProjictSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.Second_Projict_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondProjictSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondProjictSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Second_Projict_SpringBoot/SecondProjictSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Second_Projict_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondProjictSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
